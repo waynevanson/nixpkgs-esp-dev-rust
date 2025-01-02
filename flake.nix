64e622d47e88df019eb75c40002555f797d10375
Aljaž Mur Eržen <aljaz@edgedb.com>
2025-01-02 12:15:13 +0100

    esp32 rust shell

--- flake.nix
+++ flake.nix
@@ -37,6 +37,7 @@
       devShells = {
         esp-idf-full = import ./shells/esp-idf-full.nix { inherit pkgs; };
         esp32-idf = import ./shells/esp32-idf.nix { inherit pkgs; };
+        esp32-idf-rust = import ./shells/esp32-idf-rust.nix { inherit pkgs; };
         esp32c3-idf = import ./shells/esp32c3-idf.nix { inherit pkgs; };
         esp32s2-idf = import ./shells/esp32s2-idf.nix { inherit pkgs; };
         esp32s2-idf-rust = import ./shells/esp32s2-idf-rust.nix { inherit pkgs; };
---
+++ shells/esp32-idf-rust.nix
@@ -0,0 +1,42 @@
+{ pkgs ? import ../default.nix }:
+pkgs.mkShell {
+  name = "esp-idf-esp32-rust";
+
+  buildInputs = with pkgs; [
+    esp-idf-esp32
+
+    # Tools required to use ESP-IDF.
+    git
+    wget
+    gnumake
+
+    flex
+    bison
+    gperf
+    pkg-config
+    cargo-generate
+
+    cmake
+    ninja
+
+    ncurses5
+
+    llvm-xtensa
+    llvm-xtensa-lib
+    rust-xtensa
+
+    espflash
+    ldproxy
+
+    python3
+    python3Packages.pip
+    python3Packages.virtualenv
+  ];
+  shellHook = ''
+    # fixes libstdc++ issues and libgl.so issues
+    export LD_LIBRARY_PATH=${pkgs.lib.makeLibraryPath [ pkgs.libxml2 pkgs.zlib pkgs.stdenv.cc.cc.lib ]}
+    export ESP_IDF_VERSION=v4.4.1
+    export LIBCLANG_PATH=${pkgs.llvm-xtensa-lib}/lib
+    export RUSTFLAGS="--cfg espidf_time64"
+  '';
+}
